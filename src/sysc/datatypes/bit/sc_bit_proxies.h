f524c4609032ee79d9064f4b0e0c347d8a5f1076
Puneet Goel <puneet@coverify.com>
2011-05-29 23:45:43 +0530

    Fix for gcc 4.6 compilation.

--- src/sysc/datatypes/bit/sc_bit_proxies.h
+++ src/sysc/datatypes/bit/sc_bit_proxies.h
@@ -713,7 +713,7 @@ public:
 
 protected:
 
-    mutable X& m_obj;
+    X& m_obj;
     int        m_hi;
     int        m_lo;
     int        m_len;
@@ -1190,10 +1190,10 @@ public:
 
 protected:
 
-    mutable X&   m_left;
-    mutable Y&   m_right;
+    X&   m_left;
+    Y&   m_right;
     mutable int  m_delete;
-    mutable int& m_refs;
+    int& m_refs;
 
 private:
 
